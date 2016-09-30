b91822df31c781e560cd3846da03d1b9c4c2cfcb
KalyanU <kalyanuyyala@gmail.com>
2016-09-30 11:59:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,14 @@
 console.log('Loaded!');
 
+//changing text
 var element = document.getElementById('main-text');
-
 element.innerHTML = "Hello, Iam Kalyan Uyyala";
+
+//moving the image on click
+var img = document.getElementById('img');
+img.onclick = function () {
+    img.style.marginLeft = '100px';
+}
+
+
+
