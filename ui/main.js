e09b89b608dc07275f5b0bc54aae1f3647804a39
KalyanU <kalyanuyyala@gmail.com>
2016-10-03 16:49:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,21 @@ button.onclick = function(){
     request.open('GET', 'http://kalyanu.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclicked = funciton(){
+    //make the request to the server and send the name
+    
+    //capture a list of names and render as a list
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for(var i=0; i<names.length; i++){
+        list += '<li>' + name[i] + '</li>'; 
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+    
+};
