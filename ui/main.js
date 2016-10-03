7d3769fbcd63213e8d9db7ced37ca5cab6f30c89
KalyanU <kalyanuyyala@gmail.com>
2016-10-03 18:38:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,12 +31,30 @@ var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //make the request to the server and send the name
     
-    //capture a list of names and render as a list
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-    for(var i=0; i<names.length; i++){
-        list += '<li>' + names[i] + '</li>'; 
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+     
+    //create a request object
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //take some action
+            if(request.status === 200){
+                   //capture a list of names and render as a list
+                   var names = ['name1', 'name2', 'name3', 'name4'];
+                   var list = '';
+                   for(var i=0; i<names.length; i++){
+                        list += '<li>' + names[i] + '</li>'; 
+                    }
+                    var ul = document.getElementById('namelist');
+                    ul.innerHTML = list;
+                    }
+        }
+        // not done yet
+    };
+    
+    //make the request
+    request.open('GET', 'http://kalyanu.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
+
 }
