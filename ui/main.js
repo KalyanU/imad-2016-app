0c0777122a8a9edc8b2d59d4a634045df0e2d18c
KalyanU <kalyanuyyala@gmail.com>
2016-10-03 15:47:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,25 @@
-console.log('Loaded!');
+//counter code
+var button = document.getElementById('counter');
 
-//changing text
-var element = document.getElementById('main-text');
-element.innerHTML = "Hello, Iam Kalyan Uyyala";
-
-//moving the image on click
-var img = document.getElementById('madi');
-
-var marginLeft = 0; 
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-
-madi.onclick = function () {
-    var interval = setInterval(moveRight, 50);
+button.onclick = function(){
+    
+    //create a request object
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.Done){
+            //take some action
+            if(request.status === 2000){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        // not done yet
+    };
+    
+    //make the request
+    request.open('GET', 'http://kalyanu.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
-
-
-
-
